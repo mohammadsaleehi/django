6189d18fec12d5dae8f56f1dc4e94541e1687a8d
Mohammad Salehi <144056438+mohammadsaleehi@users.noreply.github.com>
2024-07-25 21:09:08 +0330

    Update test_sqlite.py

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -11,23 +11,47 @@
 # situations, so it is recommended to run the test suite against as many
 # database backends as possible.  You may want to create a separate settings
 # file for each of the backends you test against.
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        'HOST': 'localhost',
+        'NAME': 'django_test2',
+        'USER': 'mohammad3',
+        'PASSWORD': '1m2m3m4M5M',
+        'PORT': '5432'
     },
     "other": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        'HOST': 'localhost',
+        'NAME': 'django_test3',
+        'USER': 'mohammad3',
+        'PASSWORD': '1m2m3m4M5M',
+        'PORT': '5432'
     },
 }
 
-SECRET_KEY = "django_tests_secret_key"
 
-# Use a fast hasher to speed up tests.
-PASSWORD_HASHERS = [
+# settings.py
+
+WORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 USE_TZ = False
+
+#SECRET_KEY= 'eSWaAIMt2B3TBog9D8qKHSCJ-R_yZl21FMdNLOl22KXUow0Ph5y_9SesYCh-X5Yah9o'
+SECRET_KEY = "django_tests_secret_key"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://default:1m2m3m4M5M@127.0.0.1:6379",
+    }
+}
+
+#SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_ENGINE = 'django.contrib.sessions.backends.db' # New
+
+SESSION_CACHE_ALIAS = 'default'
